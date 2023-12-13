2e723f1557255bcc3507b41a9742fee3f26bed79
Alexei196 <dndy778@gmail.com>
2023-12-12 20:23:18 -0800

    added writeup

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -8,7 +8,7 @@ import torch.distributed as td
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BACKEND = "nccl" if torch.cuda.is_available() else "gloo"
 
-def training_pipeline(**args):
+def training_pipeline():
     """Container for the entire finetuning script for the Verte model. Any external call to Verte training must call this method. 
 
     Args :
@@ -20,9 +20,11 @@ def training_pipeline(**args):
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
     model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
 
+    prompt = "So how can I best use the Minstral ai for my project?"
+    inputs = tokenizer(prompt, return_tensors="pt")
+
+    generate_ids = model.generate(inputs.input_ids, max_length=1024)
+    print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser
-    parser.add_argument('-p', '--port', help="Designated port for selected distributed framework of training.", default="12055")
-    args = parser.parse_args()
-    training_pipeline(args)
+    training_pipeline()
